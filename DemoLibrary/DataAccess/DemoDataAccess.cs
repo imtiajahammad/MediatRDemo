08a58868e2fad90606f81f2c7a57cc66704970da
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-06-30 14:20:05 +0600

    dataAccess library for data access

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DemoLibrary.Models;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Tim", LastName = "Corey" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Sue", LastName = "Storm" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPeople(string firstName, string lastName)
+        {
+            PersonModel model = new() { FirstName = firstName, LastName = lastName };
+            model.Id = people.Max(x => x.Id) + 1;
+            people.Add(model);
+            return model;
+        }
+    }
+}
+
---
+++ DemoLibrary/DataAccess/IDemoDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using DemoLibrary.Models;
+
+namespace DemoLibrary.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<PersonModel> GetPeople();
+        PersonModel InsertPeople(string firstName, string lastName);
+    }
+}
