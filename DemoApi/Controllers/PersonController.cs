95feb30c5240d9aacf465cd70c00d8755fd16f81
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-07-01 19:26:11 +0600

    DemoAPI implementation

---
+++ DemoApi/Controllers/PersonController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using DemoLibrary.Commands;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public PersonController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: api/values
+        [HttpGet]
+        public async Task<List<PersonModel>> Get()
+        {
+            return await _mediator.Send(new GetPersonListQuery());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<PersonModel> Get(int id)
+        {
+            return await _mediator.Send(new GetPersonByIdQuery(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<PersonModel> Post([FromBody]PersonModel value)
+        {
+            return await _mediator.Send(new InsertPersonCommand(value.FirstName, value.LastName));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ DemoApi/Program.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using DemoLibrary;
+using DemoLibrary.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IDataAccess, DemoDataAccess>();
+builder.Services.AddMediatR(typeof(DemoLibraryMediatREntrypoint).Assembly);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+/*
+ 
+ https://www.youtube.com/watch?v=yozD5Tnd8nw&t=953s
+ 
+ */
